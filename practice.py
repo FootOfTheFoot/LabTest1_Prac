d95aa65ec349efd1bbd8741cd5446993771ba57b
Nathan <nathanchew.24@ichat.sp.edu.sg>
2025-05-23 19:48:55 +0800

    temp arr getter using split

--- practice.py
+++ practice.py
@@ -12,11 +12,28 @@ def getTemp():
         arr.append(temp)
     return arr
 
+def getTempArr():
+    tempArr=[]
+    arrString=input("Enter temp values. E.g. 3,6,8,9,22: ")
+    tempArrString=arrString.split(",")
+    for x in tempArrString:
+        tempArr.append(int(x))
+    return tempArr
+
 def main():
     tempArr=[]
-    tempArr=getTemp()
-    avg=avgTemp(tempArr)
-    print("The average temp is",avg)
+    choice=input("1 for indiv input, 2 for one shot input: ")
+    if choice=="1":
+        tempArr=getTemp()
+        avg=avgTemp(tempArr)
+        print("The average temp is",avg)
+    elif choice=="2":
+        tempArr=getTempArr()
+        avg=avgTemp(tempArr)
+        print("The average temp is",avg)
+    else:
+        print("Invalid input")
+    
 
 if __name__=='__main__':
     main()
